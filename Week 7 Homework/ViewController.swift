c1ac984b844460ad845b8f08f57e727a6e14ea64
Kevin French <KFrench1123@gmail.com>
2019-04-06 19:08:10 -0500

    Initial Commit

---
+++ Week 7 Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week 7 Homework
+//
+//  Created by Kevin French on 4/6/19.
+//  Copyright © 2019 Kevin French. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
